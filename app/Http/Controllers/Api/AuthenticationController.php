eb5dce10fed61cdba60f9b7a904fb3e655a09fb7
Dushmanta <dushmanta0511@gmail.com>
2024-05-16 23:59:50 +0530

    feat: add otp generation

--- app/Http/Controllers/Api/AuthenticationController.php
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -5,21 +5,21 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Mail\OtpVerificationEmail;
 use App\Models\User;
+use App\Models\Verification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 
 class AuthenticationController extends Controller
 {
     public function register(Request $request)
     {
-        info($request);
         $validator = Validator::make($request->all(), [
             'name' => 'required | string | min:2',
             'email' => 'required | email | unique:users',
             'password' => 'required | string | min:6'
         ]);
+
         if ($validator->fails()) {
             return response()->json(['error' => $validator->errors()], 400);
         }
@@ -30,8 +30,29 @@ class AuthenticationController extends Controller
             'password' => Hash::make($request->password),
         ]);
 
-        Mail::to($request->email)->send(new OtpVerificationEmail());
+        $otp = $this->generateAndStoreOtp($user->email);
 
-        return response()->json(['data' => ['message' => 'User registered successfully.', 'data' => $user]], 200);
+        // Mail::to($request->email)->send(new OtpVerificationEmail());
+
+        return response()->json(['data' => ['message' => 'User registered successfully.', 'data' => $user, 'otp'  => $otp]], 200);
+    }
+
+    public function login(Request $request)
+    {
+    }
+
+    public function generateAndStoreOtp($email)
+    {
+        $otp = rand(100000, 999999);
+
+        Verification::updateOrCreate(
+            ['email' => $email],
+            [
+                'email' => $email,
+                'otp' => $otp,
+            ]
+        );
+
+        return $otp;
     }
 }
